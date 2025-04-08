4c4997969bdaec54113d59735a58959fb54daea8
unknown <xsync80@myvcl.dev>
2025-04-08 21:51:50 +0200

    Limpieza de código
    Limpieza de código

--- PrecioCoinPub/PrecioVariasCoins.py
+++ PrecioCoinPub/PrecioVariasCoins.py
@@ -14,7 +14,6 @@ api_key= config.api_key
 tiempoEspera = config.milesegundosCoins
 
 
-
 # Configura la URL de la API de CoinGecko
 url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,cardano&vs_currencies=usd"
 
@@ -25,30 +24,19 @@ btc_target_price_dow = config.btc_price_down
 eth_target_price_up = config.eth_price_up # Cambia este valor al precio que desees para Ethereum
 eth_target_price_down = config.eth_price_down
 
-
 sol_target_price = config.sol_price # Cambia este valor al precio que desees para Solana
 ada_target_price = config.ada_price # Cambia este valor al precio que desees para Cardano (ADA)
-
-
-
 
 def get_crypto_prices():
     response = requests.get(url)
     data = response.json()
     return data["bitcoin"]["usd"], data["ethereum"]["usd"], data["solana"]["usd"], data["cardano"]["usd"]
 
-
 def mensajePercioBTC(btc_precio,estado):
-
     print("BTC")
-    
-    
     if estado==True:
-
         p_btc = btc_precio
         message= f"Precio BTC UP {p_btc} $ actualmente. :D "
-
-        ##print(f"El precio actual de Bitcoin es: ${btc_price}")
         
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
@@ -60,16 +48,13 @@ def mensajePercioBTC(btc_precio,estado):
             print('Mensaje enviado exitosamente 5A!')
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
-
     elif estado==False:
         p_btc =str(btc_precio)
         message=f"Precio BTC DOWN {p_btc}$ actualmente. :D "
-
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
-            'text': message
-            
+            'text': message            
         }
         response = requests.post(url, params=params)
         if response.status_code == 200:
@@ -77,19 +62,10 @@ def mensajePercioBTC(btc_precio,estado):
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
 
-
 def mensajePercioETH(ETH_precio,estado):
-
-    print("ETH")
-    
-    
-    if estado==True:
-
-        ##p_btc = eth_precio
+    print("ETH") 
+    if estado==True:    
         message= f"Precio ETH UP {ETH_precio} $ actualmente. :D "
-
-        ##print(f"El precio actual de Bitcoin es: ${btc_price}")
-        
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
@@ -102,35 +78,24 @@ def mensajePercioETH(ETH_precio,estado):
             print(f'Error al enviar el mensaje: {response.status_code}')
 
     elif estado==False:
-        
-
         message=f"Precio ETH DOWN {ETH_precio}$ actualmente. :D "
-
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
-            'text': message
-            
+            'text': message    
         }
         response = requests.post(url, params=params)
         if response.status_code == 200:
             print('Mensaje enviado exitosamente 5B!')
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
-
 
 def mensajePercioSOL(SOL_precio,estado):
 
     print("SOL")
-    
-    
+
     if estado==True:
-
-        ##p_btc = eth_precio
         message= f"Precio ETH UP {SOL_precio} $ actualmente. :D "
-
-        ##print(f"El precio actual de Bitcoin es: ${btc_price}")
-        
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
@@ -142,37 +107,25 @@ def mensajePercioSOL(SOL_precio,estado):
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
 
-    elif estado==False:
-        
-
+    elif estado==False:      
         message=f"Precio ETH DOWN {SOL_precio}$ actualmente. :D "
-
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
-            'text': message
-            
+            'text': message    
         }
         response = requests.post(url, params=params)
         if response.status_code == 200:
             print('Mensaje enviado exitosamente 5B!')
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
-
 
 
 def mensajePercioADA(ADA_precio,estado):
 
     print("ADA")
-    
-    
     if estado==True:
-
-        ##p_btc = eth_precio
-        message= f"Precio ETH UP {ADA_precio} $ actualmente. :D "
-
-        ##print(f"El precio actual de Bitcoin es: ${btc_price}")
-        
+        message= f"Precio ETH UP {ADA_precio} $ actualmente. :D "   
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
@@ -184,11 +137,8 @@ def mensajePercioADA(ADA_precio,estado):
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
 
-    elif estado==False:
-        
-
+    elif estado==False:    
         message=f"Precio ETH DOWN {ADA_precio}$ actualmente. :D "
-
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
         params = {
             'chat_id': chat_id,
@@ -201,12 +151,9 @@ def mensajePercioADA(ADA_precio,estado):
         else:
             print(f'Error al enviar el mensaje: {response.status_code}')
 
-
-
 def main():
 
     while True:
-
         btc_price, eth_price, sol_price, ada_price = get_crypto_prices()
         print(f"El precio actual de Bitcoin es: ${btc_price}")
         print(f"El precio actual de Ethereum es: ${eth_price}")
@@ -219,41 +166,32 @@ def main():
             print(f"¡Alerta! El precio de Bitcoin ha alcanzado o superado los ${btc_target_price_up}")
             ##mensajePercioBTC_UP(btc_price)
             mensajePercioBTC(btc_price, True)
-            # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
-        
+            # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.    
         if btc_price <= btc_target_price_dow:
             print(f"¡Alerta! El precio de Bitcoin ha perdido los ${btc_target_price_dow}")
             ##mensajePercioBTC_DOWN(btc_price)
             mensajePercioBTC(btc_price, False)
-            # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
-        
+            # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.  
         if eth_price >= eth_target_price_up:
             print(f"¡Alerta! El precio de Ethereum ha alcanzado o superado los ${eth_target_price_up}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
             mensajePercioETH(eth_price,True)
-
         if eth_price <= eth_target_price_down:
             print(f"¡Alerta! El precio de Ethereum ha pertidido los ${eth_target_price_down}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
-            mensajePercioETH(eth_price,False)
-        
-        
+            mensajePercioETH(eth_price,False)        
         if sol_price >= sol_target_price:
             print(f"¡Alerta! El precio de Solana ha alcanzado o superado los ${sol_target_price}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
             mensajePercioSOL(sol_price,True)
-
         if sol_price < sol_target_price:
             print(f"¡Alerta! El precio de Solana ha alcanzado o superado los ${sol_target_price}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
             mensajePercioSOL(sol_price,False)
-
-
         if ada_price >= ada_target_price:
             print(f"¡Alerta! El precio de Cardano (ADA) ha alcanzado o superado los ${ada_target_price}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
-            mensajePercioADA(ada_price,True)
-        
+            mensajePercioADA(ada_price,True)        
         if ada_price < ada_target_price:
             print(f"¡Alerta! El precio de Cardano (ADA) ha alcanzado o superado los ${ada_target_price}")
             # Aquí puedes añadir código para enviar un correo electrónico, mensaje de texto, etc.
